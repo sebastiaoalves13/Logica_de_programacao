d0d17a55379d5ea23bed44036c8d4aed91ebdef2
sebastiaoalves13 <42155385+sebastiaoalves13@users.noreply.github.com>
2018-08-23 21:25:37 -0300

    Add files via upload

---
+++ sistemaBancario.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class sistemaBancario {
+    public static void main(String[]args){
+        Scanner tcl = new Scanner(System.in);
+        double saldo , deposito , saque;
+        int sair;
+        System.out.println("digete 1 para saber seu saldo");
+        System.out.println("digite 2 para executar um deposito");
+        System.out.println("digite tres para sacar um valor");
+        System.out.println("Sigite 4 para sair da sua conta");
+        int aao =tcl.nextInt();
+        if(aao==4){
+            System.out.print("xau");
+        }
+        ;
+    
+    
+  
+       
+   } 
+}
+
